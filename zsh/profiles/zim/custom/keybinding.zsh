62d25c0e2c9b8737ce20a0b0b7bc50d98988ac75
Hermit <persona_gazer@outlook.com>
2025-11-27 09:38:55 +0900

    ADD: Init commit
    Add zim framework to config zsh
    Add fzf config
    Add custom keybindings

---
+++ zsh/profiles/zim/custom/keybinding.zsh
@@ -0,0 +1,7 @@
+alias nv='nvim'
+alias lg='lazygit'
+
+bindkey -M viins '^A' beginning-of-line
+bindkey -M viins '^E' end-of-line
+bindkey -M viins '^W' backward-kill-word
+bindkey -M viins '^U' backward-kill-line
