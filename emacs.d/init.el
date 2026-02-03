610dedc9c0a236291048b6eea87bfcc7bc54f3fa
Hermit <persona_gazer@outlook.com>
2026-02-03 22:56:53 +0900

    MOD: Fix multiple cursors & Add org-agenda config.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,9 +1,10 @@
+(require 'package)
 (eval-when-compile
   (require 'use-package))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(setq custom-file "/home/hermit/.emacs.d/emacs-auto-custom.el")
+(setq custom-file "~/.emacs.d/emacs-auto-custom.el")
 (load-file custom-file)
 
 (tool-bar-mode 0)
@@ -35,10 +36,17 @@
 (keymap-global-set "C-j C-k" 'kill-whole-line)
 (keymap-global-set "M-/" 'hippie-expand)
 
+
 (use-package counsel
   :ensure t
   :after ivy
   :config (counsel-mode 1))
+
+(use-package ivy-prescient
+  :ensure t
+  :after counsel
+  :config
+  (ivy-prescient-mode 1))
 
 (use-package ivy
   :ensure t
@@ -105,7 +113,7 @@ Up^^             Down^^           Miscellaneous           % 2(mc/num-cursors) cu
  [_P_]   Skip     [_N_]   Skip     [_a_] Mark all    [_A_] Insert letters
  [_M-p_] Unmark   [_M-n_] Unmark   [_s_] Search      [_q_] Quit
  [_|_] Align with input CHAR       [Click] Cursor at point"
-		  ("l" mc/edit-lines :exit t)
+		  ("l" mc/edit-lines)
 		  ("a" mc/mark-all-like-this :exit t)
 		  ("n" mc/mark-next-like-this)
 		  ("N" mc/skip-to-next-like-this)
@@ -154,6 +162,130 @@ Up^^             Down^^           Miscellaneous           % 2(mc/num-cursors) cu
   (setq truncate-lines nil)
   :hook
   (prog-mode . flycheck-mode))
+
+(use-package org-roam
+  :ensure t
+  :after org
+  :init
+  (setq org-roam-v2-ack t)
+  :config
+  (org-roam-setup)
+  :custom
+  (org-roam-directory (concat org-directory "roam/"))
+  :bind
+  (("C-c n f" . org-roam-node-find)
+   (:map org-mode-map
+	 (("C-c n i" . org-roam-node-insert)
+	  ("C-c n o" . org-id-get-create)
+	  ("C-c n t" . org-roam-tag-add)
+	  ("C-c n a" . org-roam-alias-add)
+	  ("C-c n l" . org-roam-buffer-toggle)))))
+
+
+(setq org-directory (file-truename "~/org/"))
+(setq pv/org-agenda-files `(,(concat org-directory "agenda/")))
+
+(use-package org
+  :init
+  (require 'org-indent)
+  :config
+  (defun pv/init-org-hook ()
+    (setq truncate-lines nil)
+    (org-toggle-pretty-entities)) ; display LaTeX symbols
+  (defun pv/org-skip-subtree-if-priority (priority)
+    "Skip an agenda subtree if it has a priority of PRIORITY.
+
+PRIORITY may be one of the characters ?A, ?B, or ?C."
+    (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+          (pri-value (* 1000 (- org-lowest-priority priority)))
+          (pri-current (org-get-priority (thing-at-point 'line t))))
+      (if (= pri-value pri-current)
+          subtree-end
+        nil)))
+  (defun pv/org-skip-subtree-if-habit ()
+    "Skip an agenda entry if it has a STYLE property equal to \"habit\"."
+    (let ((subtree-end (save-excursion (org-end-of-subtree t))))
+      (if (string= (org-entry-get nil "STYLE") "habit")
+          subtree-end
+        nil)))
+  :hook
+  (org-mode . pv/init-org-hook)
+  :custom
+  (org-hide-leading-stars t "clearer way to display")
+  (org-startup-with-inline-images t "always display inline image")
+  (org-image-actual-width 600 "set width of image when displaying")
+  (org-outline-path-complete-in-steps nil)
+  (org-todo-keywords
+   (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+           (sequence "WAITING(w@/!)" "|" "CANCELLED(c@/!)" "MEETING"))))
+  (org-todo-keyword-faces
+   (quote (("TODO" :foreground "goldenrod1" :weight bold)
+           ("NEXT" :foreground "DodgerBlue1" :weight bold)
+           ("DONE" :foreground "SpringGreen2" :weight bold)
+           ("WAITING" :foreground "LightSalmon1" :weight bold)
+           ("CANCELLED" :foreground "LavenderBlush4" :weight bold)
+           ("MEETING" :foreground "IndianRed1" :weight bold))))
+  (org-todo-state-tags-triggers
+   (quote (("CANCELLED" ("CANCELLED" . t))
+           ("WAITING" ("WAITING" . t))
+           (done ("WAITING"))
+           ("TODO" ("WAITING") ("CANCELLED"))
+           ("NEXT" ("WAITING") ("CANCELLED"))
+           ("DONE" ("WAITING") ("CANCELLED")))))
+  (org-adapt-indentation t)
+  (org-agenda-files pv/org-agenda-files)
+  ;; Do not dim blocked tasks
+  (org-agenda-dim-blocked-tasks nil)
+  ;; compact the block agenda view
+  (org-agenda-compact-blocks t)
+  (org-agenda-span 7)
+  (org-agenda-start-day "-2d")
+  (org-agenda-start-on-weekday nil)
+  (org-agenda-tags-column -86) ; default value auto has issues
+  ;; Custom agenda command definitions
+  (org-agenda-custom-commands
+   (quote (("d" "Daily agenda and all TODOs"
+            ((tags "PRIORITY=\"A\""
+                   ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                    (org-agenda-overriding-header "High-priority unfinished tasks:")))
+             (agenda "" ((org-agenda-ndays 1)))
+             (alltodo ""
+                      ((org-agenda-skip-function '(or (pv/org-skip-subtree-if-habit)
+                                                      (pv/org-skip-subtree-if-priority ?A)
+                                                      (org-agenda-skip-if nil '(scheduled deadline))))
+                       (org-agenda-overriding-header "ALL normal priority tasks:"))))
+            ((org-agenda-compact-blocks t)))
+           ("p" "Projects"
+            ((agenda "" nil)
+             (tags "REFILE"
+                   ((org-agenda-overriding-header "Tasks to Refile")
+                    (org-tags-match-list-sublevels nil)))
+             (tags-todo "-CANCELLED/!"
+                        ((org-agenda-overriding-header "Stuck Projects")
+                         (org-agenda-skip-function 'bh/skip-non-stuck-projects)
+                         (org-agenda-sorting-strategy
+                          '(category-keep))))
+             (tags-todo "-CANCELLED/!NEXT"
+                        ((org-agenda-overriding-header (concat "Project Next Tasks"
+                                                               (if bh/hide-scheduled-and-waiting-next-tasks
+                                                                   ""
+                                                                 " (including WAITING and SCHEDULED tasks)")))
+                         (org-agenda-skip-function 'bh/skip-projects-and-habits-and-single-tasks)
+                         (org-tags-match-list-sublevels t)
+                         (org-agenda-todo-ignore-scheduled bh/hide-scheduled-and-waiting-next-tasks)
+                         (org-agenda-todo-ignore-deadlines bh/hide-scheduled-and-waiting-next-tasks)
+                         (org-agenda-todo-ignore-with-date bh/hide-scheduled-and-waiting-next-tasks)
+                         (org-agenda-sorting-strategy
+                          '(todo-state-down effort-up category-keep))))
+             (tags "-REFILE/"
+                   ((org-agenda-overriding-header "Tasks to Archive")
+                    (org-agenda-skip-function 'bh/skip-non-archivable-tasks)
+                    (org-tags-match-list-sublevels nil))))
+            nil))))
+  :bind
+  (("C-c a" . 'org-agenda)
+   :map org-mode-map
+   ("C-c C-q" . counsel-org-tag)))
 
 
 ;;; init.el ends here
